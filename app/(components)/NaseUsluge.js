90756fcf6783732c862534bec45a75d5f08a6d7d
Aleks <aleksandarhough@gmail.com>
2024-01-12 12:41:41 +0100

    fix

--- app/(components)/NaseUsluge.js
+++ app/(components)/NaseUsluge.js
@@ -9,9 +9,9 @@ import {
 
 function NaseUsluge(props) {
   return (
-    <div className="bg-black border-t-1 border-black flex flex-col justify-center items-center h-screen w-screen">
+    <div className="bg-black border-t-1 border-black flex flex-col justify-center items-center h-screen w-full overflow-hidden">
       <h1 className="mt-0 text-white mb-16 text-3xl">Zašto EOUS?</h1>
-      <div className="w-4/5 h-3/5 grid grid-cols-2 gap-8 justify-center mx-auto">
+      <div className=" gap-4 p-4 md:w-4/5 h-3/5 grid grid-cols-2 md:gap-8 justify-center mx-auto w-full">
         <Card title="BRZO" text="Tekst" animationColor={props.animationColor}>
           <IconBolt size={props.iconSize} strokeWidth={2} color={"#5005FF"} />
         </Card>
@@ -34,7 +34,7 @@ function NaseUsluge(props) {
           />
         </Card>
         <Card
-          title="DECENTRALIZOVANO"
+          title="DE-CENTRALIZOVANO"
           text="Tekst"
           animationColor={props.animationColor}
         >
