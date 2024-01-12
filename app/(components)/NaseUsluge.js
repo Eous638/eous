60336dd36f5e5f1ebb5a7b743cac93e29706361a
Aleks <aleksandarhough@gmail.com>
2024-01-12 12:13:26 +0100

    bla

--- app/(components)/NaseUsluge.js
+++ app/(components)/NaseUsluge.js
@@ -7,62 +7,46 @@ import {
   IconBinaryTree,
 } from "@tabler/icons-react";
 
-export default function NaseUsluge(props) {
+function NaseUsluge(props) {
   return (
-    <div
-      style={{
-        backgroundColor: "#19191a",
-        borderTop: "0.1rem solid #19191a",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100vw",
-      }}
-    >
-      <h1
-        style={{
-          marginTop: "0",
-          color: "white",
-          marginBottom: "4rem",
-          fontSize: "3rem",
-        }}
-      >
-        Zašto EOUS?
-      </h1>
-      <div
-        style={{
-          width: "80vw",
-          height: "60vh",
-          display: "grid",
-          gridTemplateColumns: "repeat(2, 1fr)",
-          gap: "2rem",
-          justifyContent: "center",
-          margin: "3 auto",
-        }}
-      >
+    <div className="bg-black border-t-1 border-black flex flex-col justify-center items-center h-screen w-screen">
+      <h1 className="mt-0 text-white mb-16 text-3xl">Zašto EOUS?</h1>
+      <div className="w-4/5 h-3/5 grid grid-cols-2 gap-8 justify-center mx-auto">
         <Card title="BRZO" text="Tekst" animationColor={props.animationColor}>
-          <IconBolt size={95} strokeWidth={2} color={"#5005FF"} />
+          <IconBolt size={props.iconSize} strokeWidth={2} color={"#5005FF"} />
         </Card>
         <Card
           title="CROSS-PLATFORM"
           text="Tekst"
           animationColor={props.animationColor}
         >
-          <IconDevices size={95} strokeWidth={2} color={"#5005FF"} />
+          <IconDevices
+            size={props.iconSize}
+            strokeWidth={2}
+            color={"#5005FF"}
+          />
         </Card>
         <Card title="EDGE" text="Tekst" animationColor={props.animationColor}>
-          <IconCubeSend size={95} strokeWidth={2} color={"#5005FF"} />
+          <IconCubeSend
+            size={props.iconSize}
+            strokeWidth={2}
+            color={"#5005FF"}
+          />
         </Card>
         <Card
           title="DECENTRALIZOVANO"
           text="Tekst"
           animationColor={props.animationColor}
         >
-          <IconBinaryTree size={95} strokeWidth={2} color={"#5005FF"} />
+          <IconBinaryTree
+            size={props.iconSize}
+            strokeWidth={2}
+            color={"#5005FF"}
+          />
         </Card>
       </div>
     </div>
   );
 }
+
+export default NaseUsluge;
