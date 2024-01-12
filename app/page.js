832245b5516edef4f8a91189ce273aa41461df3d
Aleks <aleksandarhough@gmail.com>
2024-01-12 13:33:31 +0100

    fix window

--- app/page.js
+++ app/page.js
@@ -5,11 +5,13 @@ import Form from "./(components)/Form";
 import NaseUsluge from "./(components)/NaseUsluge";
 import { useEffect, useState } from "react";
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
+  if (typeof window === "undefined") {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }
 }
 
 function useWindowDimensions() {
